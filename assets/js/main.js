6d4da18e2430f84ef30f4b045690897f9a6f4240
mullahbabs <bennybeshel@gmail.com>
2024-12-04 03:03:43 +0100

    s

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
   
 /* 1. Proloder */
     $(window).on('load', function () {
-      $('#preloader-active').delay(450).fadeOut('fast');
+      $('#preloader-active').delay(450).fadeOut('slow');
       $('body').delay(450).css({
         'overflow': 'visible'
       });
